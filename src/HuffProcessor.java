7bd9de5be0df16efc5a0d228533adfa0d0283568
jh603 <jerry.huang176@duke.edu>
2018-12-07 01:22:07 -0500

    a short description of your commit here

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -124,6 +124,7 @@ public class HuffProcessor {
 	   while(true) {
 	      int bits = in.readBits(BITS_PER_WORD);
 	      code = encoding[bits];
+	      System.out.println(code.length());
 	      out.writeBits(code.length(), Integer.parseInt(code, 2));
 	      code = encoding[PSEUDO_EOF];
 	      out.writeBits(code.length(), Integer.parseInt(code, 2));
