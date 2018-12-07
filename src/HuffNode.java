e58c0d23f8aabce3dc82f8e705232ae97d71996f
jh603 <jerry.huang176@duke.edu>
2018-12-07 17:08:58 -0500

    a short description of your commit here

--- src/HuffNode.java
+++ src/HuffNode.java
@@ -10,53 +10,53 @@
 
 public class HuffNode implements Comparable<HuffNode> {
 
-	public int myValue, myWeight;
-	public HuffNode myLeft, myRight;
+   public int myValue, myWeight;
+   public HuffNode myLeft, myRight;
 
-	/**
-	 * construct leaf node (null children)
-	 * 
-	 * @param value
-	 *            is the value stored in the node (e.g., character)
-	 * @param weight
-	 *            is used for comparison (e.g., count of # occurrences)
-	 */
-	public HuffNode(int value, int weight) {
-		this(value, weight, null, null);
-	}
+   /**
+    * construct leaf node (null children)
+    * 
+    * @param value
+    *            is the value stored in the node (e.g., character)
+    * @param weight
+    *            is used for comparison (e.g., count of # occurrences)
+    */
+   public HuffNode(int value, int weight) {
+      this(value, weight, null, null);
+   }
 
-	/**
-	 * construct internal node (with children)
-	 * 
-	 * @param value
-	 *            is stored as value of node
-	 * @param weight
-	 *            is weight of node
-	 * @param ltree
-	 *            is left subtree
-	 * @param rtree
-	 *            is right subtree
-	 */
-	public HuffNode(int value, int weight, HuffNode left, HuffNode right) {
-		myValue = value;
-		myWeight = weight;
-		myLeft = left;
-		myRight = right;
-	}
+   /**
+    * construct internal node (with children)
+    * 
+    * @param value
+    *            is stored as value of node
+    * @param weight
+    *            is weight of node
+    * @param ltree
+    *            is left subtree
+    * @param rtree
+    *            is right subtree
+    */
+   public HuffNode(int value, int weight, HuffNode left, HuffNode right) {
+      myValue = value;
+      myWeight = weight;
+      myLeft = left;
+      myRight = right;
+   }
 
-	/**
-	 * Return value based on comparing this HuffNode to another
-	 * BASED ON WEIGHT.
-	 * 
-	 * @return -1 if this < o, +1 if this > o, and 0 if this == 0
-	 */
-	@Override
-	public int compareTo(HuffNode other) {
-		return myWeight - other.myWeight;
-	}
+   /**
+    * Return value based on comparing this HuffNode to another
+    * BASED ON WEIGHT.
+    * 
+    * @return -1 if this < o, +1 if this > o, and 0 if this == 0
+    */
+   @Override
+   public int compareTo(HuffNode other) {
+      return myWeight - other.myWeight;
+   }
 
-	@Override
-	public String toString() {
-		return Character.toString((char)myValue);
-	}
+   @Override
+   public String toString() {
+      return Character.toString((char)myValue);
+   }
 }
